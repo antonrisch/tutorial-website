a3fea3f6926512eaea6f4b71cf3788ea2741f67f
antonrisch <152280372+antonrisch@users.noreply.github.com>
2023-12-16 16:38:37 +1000

    Update actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,9 @@
 'use server';
 
 import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
  
 const FormSchema = z.object({
   id: z.string(),
@@ -19,5 +22,14 @@ export async function createInvoice(formData: FormData) {
     status: formData.get('status'),
   };
   // Test it out:
-  console.log(rawFormData);
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+   await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+   revalidatePath('/dashboard/invoices');
+   redirect('/dashboard/invoices');
 }
