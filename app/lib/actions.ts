45de92be6de33faa9a07d0b8823ed580e03399a0
antonrisch <152280372+antonrisch@users.noreply.github.com>
2023-12-16 17:54:48 +1000

    Update actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,4 +1,5 @@
-'use server';
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
@@ -32,4 +33,24 @@ export async function createInvoice(formData: FormData) {
 
    revalidatePath('/dashboard/invoices');
    redirect('/dashboard/invoices');
+}
+
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
